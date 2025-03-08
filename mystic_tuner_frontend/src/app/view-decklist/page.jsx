2c2129b68663bf91fd6eef84bf9237c7f57480a6
Cody Casselman <cody.casselman@ucalgary.ca>
2025-03-08 12:05:48 -0700

    Modified: Added commenting
    Added comments and performed slight refactor to loginDropdown

--- mystic_tuner_frontend/src/app/view-decklist/page.jsx
+++ mystic_tuner_frontend/src/app/view-decklist/page.jsx
@@ -1,4 +1,4 @@
-export default function ViewUserDecks(){
+export default function ViewDeckList(){
     return(
         <div></div>
     );
--- mystic_tuner_frontend/src/components/loginDropdown.jsx
+++ mystic_tuner_frontend/src/components/loginDropdown.jsx
@@ -1,20 +1,31 @@
-"use client";
+"use client"; //Must be a client component due to its necessity to be reloaded
 import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Button } from "@heroui/react";
 import Image from "next/image";
 
 export default function LoginDropdown(props){
+    /**
+     * The login dropdown is a dropdown menu that will appear on the top right of the universal header element.
+     * It allows for auth0 routing to handle the user session and authentication.
+     * 
+     */
     const iconWidth=50;
     const iconHeight=50;
     const session = props.session;
 
     const dropdownItems = 
+    /**dropdownItems contains information about each potential item.
+    * key: rqeuired for the dropdownMenu component. Unique identifier for the item
+    * label: The text to be displayed with the item
+    * type: allows specific handling of the item based on its type. (NOTE: this is not a typescript custom type)
+    * link: allows for dynamic routing depending on the dropdown item
+    */
     session ? [
         {key:"username", label:"Hello, " + session.user.name, type:"label"},
-        {key:"view-user-decks", label:"Your Decks", type:"link"},
-        {key:"logout",label:"Sign Out",type:"link"}
+        {key:"view-user-decks", label:"Your Decks", type:"link", link:"/view-user-decks"},
+        {key:"logout",label:"Sign Out",type:"link", link:"/auth/logout"}
     ] : [
         {key:"guest",label:"Guest",type:"label"},
-        {key:"login",label:"Login / Sign Up",type:"link"},
+        {key:"login",label:"Login / Sign Up",type:"link", link:"/auth/login"},
     ]
 
     return(
@@ -25,7 +36,7 @@ export default function LoginDropdown(props){
                         <Image className="m-0 p-0" src='userIconLight.svg' width={iconWidth} height={iconHeight} alt='Light User Icon'/>
                     </Button>
                 </DropdownTrigger>
-                <DropdownMenu 
+                <DropdownMenu /**Dropdown menu will be populated dynamically depending on the contents of dropdownItems */
                     aria-label="Dynamic Menu" 
                     classNames={{
                         list:"w-200 list-none m-2 p-2"
@@ -40,14 +51,16 @@ export default function LoginDropdown(props){
 }
 
 const buildDropdownItem = (item) =>{
-     const pageLinks = {"logout" : "/auth/logout", "login" : "/auth/login", "view-user-decks": "/view-user-decks"}
+    /**
+     * buildDropdownItem takes an item from the dropdownItems list and returns a DropDownItem component with the designated functionality.
+     */
     return(
         <DropdownItem
             showDivider = {item.type == "label" ? true : false}
             key={item.key}
             className = {"font-sans "+ (item.type === "link" ? "text-blue underline" : "text-black")}
             color="primary"
-            href = {item.type == "label" ? null : pageLinks[item.key]}
+            href = {item.link} //this will be null if the item does not contain a link
         >{item.label}
         </DropdownItem>
     )
