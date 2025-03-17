a2caa681745070fca549dd062236770e6eb9077b
Brendan Smiley <bsmiley.lego@gmail.com>
2025-03-16 23:35:17 -0600

    Fix: forward slashes

--- src/app/api/decks/[deck-id]/route.js
+++ src/app/api/decks/[deck-id]/route.js
@@ -1,77 +1,88 @@
 import { NextResponse } from "next/server";
 
 export async function PATCH(request) {
-	try {
-		const { auth0Token, cardsAdded, cardsRemoved } = request.json();
-		const response = await updateDeck(request.params.deckId, auth0Token, cardsAdded, cardsRemoved);
-		return NextResponse.json(response);
-	} catch (error) {
-		console.error("Error updating deck:", error);
-		return NextResponse.json({
-			status: 500,
-			message: "Error updating deck",
-		});
-	}
+  try {
+    const { auth0Token, cardsAdded, cardsRemoved } = request.json();
+    const response = await updateDeck(
+      request.params.deckId,
+      auth0Token,
+      cardsAdded,
+      cardsRemoved
+    );
+    return NextResponse.json(response);
+  } catch (error) {
+    console.error("Error updating deck:", error);
+    return NextResponse.json({
+      status: 500,
+      message: "Error updating deck",
+    });
+  }
 }
 
 export async function updateDeck(deckId, auth0Token, cardsAdded, cardsRemoved) {
-	try {
-		const controller = new AbortController();
-		const timeoutId = setTimeout(() => controller.abort(), 10000); // 10 second timeout
+  try {
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), 10000); // 10 second timeout
 
-		const response = await fetch(`${process.env.BACKEND_BASE_URL}/apis/decks/${deckId}`, {
-			method: "PATCH",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ auth0Token, cardsAdded, cardsRemoved }),
-			signal: controller.signal,
-		});
-		clearTimeout(timeoutId);
+    const response = await fetch(
+      `${process.env.BACKEND_BASE_URL}/apis/decks/${deckId}/`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ auth0Token, cardsAdded, cardsRemoved }),
+        signal: controller.signal,
+      }
+    );
+    clearTimeout(timeoutId);
 
-		const data = await response.json();
-		console.log("Deck update response:", data);
-		return data;
-	} catch (error) {
-		console.error("Error updating deck:", error);
-		return { status: 500, message: "Error updating deck" };
-	}
+    const data = await response.json();
+    console.log("Deck update response:", data);
+    return data;
+  } catch (error) {
+    console.error("Error updating deck:", error);
+    return { status: 500, message: "Error updating deck" };
+  }
 }
 
 export async function GET(request) {
-	try {
-		const { auth0Token } = request.json();
-		const response = await getDeck(request.params.deckId, auth0Token);
-		return NextResponse.json(response);
-	} catch (error) {
-		console.error("Error getting deck:", error);
-		return NextResponse.json({
-			status: 500,
-			message: "Error getting deck",
-		});
-	}
+  try {
+    const { auth0Token } = request.json();
+    const response = await getDeck(request.params.deckId, auth0Token);
+    return NextResponse.json(response);
+  } catch (error) {
+    console.error("Error getting deck:", error);
+    return NextResponse.json({
+      status: 500,
+      message: "Error getting deck",
+    });
+  }
 }
 
 export async function getDeck(deckId, auth0Token) {
-	try {
-		const controller = new AbortController();
-		const timeoutId = setTimeout(() => controller.abort(), 10000); // 10 second
+  try {
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), 10000); // 10 second
 
-		const response = await fetch(`${process.env.BACKEND_BASE_URL}/api/decks/${deckId}`, {
-			method: "GET",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ auth0Token }),
-			signal: controller.signal,
-		});
-		clearTimeout(timeoutId);
+    const response = await fetch(
+      `${process.env.BACKEND_BASE_URL}/api/decks/${deckId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ auth0Token }),
+        signal: controller.signal,
+      }
+    );
+    clearTimeout(timeoutId);
 
-		const data = await response.json();
-		console.log("Deck response:", data);
-		return data;
-	} catch (error) {
-		console.error("Error getting deck:", error);
-		return { status: 500, message: "Error getting deck" };
-	}
+    const data = await response.json();
+    console.log("Deck response:", data);
+    return data;
+  } catch (error) {
+    console.error("Error getting deck:", error);
+    return { status: 500, message: "Error getting deck" };
+  }
 }
--- src/app/api/decks/commander/route.js
+++ src/app/api/decks/commander/route.js
@@ -1,52 +1,57 @@
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-	try {
-		const { commander } = await request.json();
-		console.log("Commander:", commander);
+  try {
+    const { commander } = await request.json();
+    console.log("Commander:", commander);
 
-		const controller = new AbortController();
-		const timeoutId = setTimeout(() => controller.abort(), 10000); // 10 second timeout
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), 10000); // 10 second timeout
 
-		const response = await fetch(`${process.env.BACKEND_BASE_URL}/api/decks/commander`, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ commander }),
-			signal: controller.signal,
-		});
-		clearTimeout(timeoutId);
+    const response = await fetch(
+      `${process.env.BACKEND_BASE_URL}/api/decks/commander/`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ commander: commander }),
+        signal: controller.signal,
+      }
+    );
+    clearTimeout(timeoutId);
 
-		console.log("Raw response text:", response.statusText);
-		console.log("Response status:", response.status);
+    console.log("Raw response text:", response.statusText);
+    console.log("Response status:", response.status);
 
-		if (response.status === 422) {
-			// Return bad request status and empty commander
-			return NextResponse.json({
-				status: 422,
-				statusText: response.statusText,
-			});
-		} else {
-			const data = await response.json();
-			console.log("Commander:", data.commander);
-			return NextResponse.json({
-				status: response.status,
-				statusText: response.statusText,
-				commander: data.commander,
-				images: data.images ? {
-					small: data.images.small,
-					normal: data.images.normal,
-					large: data.images.large,
-					art_crop: data.images.art_crop,
-				} : null
-			});
-		}
-	} catch (error) {
-		console.error("Error processing commander:", error);
-		return NextResponse.json({
-			status: 500,
-			message: "Error processing commander",
-		});
-	}
+    if (response.status === 422) {
+      // Return bad request status and empty commander
+      return NextResponse.json({
+        status: 422,
+        statusText: response.statusText,
+      });
+    } else {
+      const data = await response.json();
+      console.log("Commander:", data.commander);
+      return NextResponse.json({
+        status: response.status,
+        statusText: response.statusText,
+        commander: data.commander,
+        images: data.images
+          ? {
+              small: data.images.small,
+              normal: data.images.normal,
+              large: data.images.large,
+              art_crop: data.images.art_crop,
+            }
+          : null,
+      });
+    }
+  } catch (error) {
+    console.error("Error processing commander:", error);
+    return NextResponse.json({
+      status: 500,
+      message: "Error processing commander",
+    });
+  }
 }
