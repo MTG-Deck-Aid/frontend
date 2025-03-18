3f7040e2b6dd526c4eddad3b9b9e2bf1f879d922
Cody Casselman <cody.casselman@ucalgary.ca>
2025-03-18 15:59:49 -0600

    Add: front page more responsive

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,10 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '@heroui/button';
+import { Divider } from "@heroui/react";
 import './globals.css';
 import UserDecks from '@/components/userDecks';
-import pageLogo from '/public/logo-dark.png';
+import pageLogo from '/public/MysticTuner-Logo.svg';
 
 export default async function Home() {
 	const logoWidth = 350;
@@ -12,13 +13,13 @@ export default async function Home() {
 	return (
 		<main className="m-2 mb-10 p-2 sm:m-8 sm:mb-20 sm:p-8">
 			<div className="flex basis-full flex-col gap-4">
-				<div className="flex justify-center">
+				<div className="flex flex-shrink justify-center">
 					<Image
 						src={pageLogo}
 						width={logoWidth}
 						height={logoHeight}
 						alt="Mystic Tuner logo"
-						className="self-center w-full max-w-[350px] h-full max-h-[350px]"
+						className="self-center w-full max-w-[350px] h-auto"
 					/>
 				</div>
 				<div className="flex justify-center">
@@ -36,6 +37,7 @@ export default async function Home() {
 						Make a Deck!
 					</Button>
 				</div>
+				<Divider/>
 				<div className="mt-10 w-full">
 					<UserDecks />
 				</div>
