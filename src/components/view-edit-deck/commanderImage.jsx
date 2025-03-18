f8da1c966200386cb3ca4946637ce5f5ddd97852
lujarios <144175748+Lujarios@users.noreply.github.com>
2025-03-18 13:25:47 -0600

    CommanderImage on Edit Page

--- src/components/view-edit-deck/commanderImage.jsx
+++ src/components/view-edit-deck/commanderImage.jsx
@@ -1,29 +1,70 @@
 "use client";
+import { useState, useEffect } from "react";
 import { useViewDeckContext } from "../viewDeckContextProvider";
 import Image from "next/image";
 import NameAutocomplete from "./nameAutocomplete";
 import emptyCommanderFrame from "/public/emptyCommander.svg";
 
-export default function CommanderImage(){
-    const{isEditMode} = useViewDeckContext();   
-    
+export default function CommanderImage() {
+    const { isEditMode } = useViewDeckContext();
+    const [commander, setCommanderName] = useState(""); //TODO: put in default commander value
+    const [commanderImage, setCommanderImage] = useState(emptyCommanderFrame);
+
+    const handleNameChange = (name) => {
+        setCommanderName(name);
+    };
+
+    const fetchAndSetCommanderImage = async (name) => {
+        let input = { commander: name };
+        let data = null;
+        try {
+            const response = await fetch('/api/decks/commander', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(input)
+            });
+            data = await response.json();
+            console.log('Commander Image Data: ', data);
+        } catch (error) {
+            console.error('Error fetching commander image: ', error);
+        }
+
+        if (data.status === 200) {
+            try {
+                let image = data.images.normal;
+                console.log('Commander Image: ', image);
+                setCommanderImage(image);
+            } catch (error) {
+                console.error('Error setting commander image: ', error);
+            }
+        }
+    }
+
+    useEffect(() => {
+        console.log('Commander Returned: ', commander);
+        console.log('Fetching Commander Image');
+        fetchAndSetCommanderImage(commander);
+    }, [commander]);
+
     //Page constants
     const imageHeight = 350;
     const imageWidth = 250;
 
-    return(
-        isEditMode?( 
+    return (
+        isEditMode ? (
             <div className="relative flex justify-center items-center w-full max-w-md">
                 <Image
-                    src={emptyCommanderFrame}
+                    src={commanderImage}
                     width={imageWidth}
                     height={imageHeight}
                     alt="Empty Commander Frame"
                 />
 
-                <NameAutocomplete />
+                <NameAutocomplete onNameChange={handleNameChange} />
             </div>
-        ):(
+        ) : (
             <div className="flex flex-col justify-center items-center w-full max-w-md">
                 <p>Commander Name</p>
                 <Image
--- src/components/view-edit-deck/nameAutocomplete.jsx
+++ src/components/view-edit-deck/nameAutocomplete.jsx
@@ -1,72 +1,75 @@
 import { Autocomplete, AutocompleteItem, Button } from "@heroui/react";
 import { useAsyncList } from "@react-stately/data";
 import { getCards } from "@/app/api/cards/autocomplete/route";
-import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure} from "@heroui/modal";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure } from "@heroui/modal";
 import { useState } from "react";
 
-export default function NameAutocomplete(){
-    const{isOpen, onOpen, onOpenChange} = useDisclosure(); //disclosure handles the state of the modal
+export default function NameAutocomplete({ onNameChange }) {
+    const { isOpen, onOpen, onOpenChange } = useDisclosure(); //disclosure handles the state of the modal
     const [commander, selectCommander] = useState("temp"); //TODO: put in default commander value
     let list = useAsyncList({
         //useAsyncList allows for a dynamic list to be updated whenever we change the filtertext
         //ref: https://react-spectrum.adobe.com/react-stately/useAsyncList.html
-        async load({filterText}){
-            {/**This func is called whenever filterText is changed */}
+        async load({ filterText }) {
+            {/**This func is called whenever filterText is changed */ }
             let cards = await getCards(filterText);
-            return{
+            return {
                 items: cards
             };
         }
     });
     const onSuccess = () => {
         console.log(commander);
+        onNameChange(commander);
         onOpenChange(); //closes modal
     }
-    return(
+    return (
         <>
-        <Button
-            className="max-w-xs absolute w-[300px] h-[80px]"
-            onPress={onOpen}
-            color={"primary"}
-            variant={"faded"}
-        >Autocomplete
-        </Button>
-        
-        <Modal isOpen={isOpen} placement="top-center" hideCloseButton={true} onOpenChange={onOpenChange} size="md">
-            <ModalContent>
-                {(onClose) => (
-                    <>
-                    <ModalHeader className="flex flex-col p-2 m-2">Find a Commander!</ModalHeader>
-                    <ModalBody className="flex flex-col m-2 py-0">
-                        <Autocomplete
-                        label="Select Commander"
-                        placeholder="Type to search..."
-                        inputProps={{
-                            classNames:{label:"my-0 relative"}
-                        }}
-                        items={list.items}
-                        inputValue={list.filterText}
-                        isLoading = {list.isLoading}
-                        onInputChange={list.setFilterText}
-                        onSelectionChange={selectCommander} //will call specified method with selected item's key (in this case key=item= commanders name)
-                        >
-                        {list.items.map((item) => {return (
-                            <AutocompleteItem className="" key={item}>
-                                {item}
-                            </AutocompleteItem>
-                        )})}
-                        </Autocomplete>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button color="success" variant="flat" onPress={onSuccess} > {/** Make sure whatever func this calls also
+            <Button
+                className="max-w-xs absolute w-[300px] h-[80px]"
+                onPress={onOpen}
+                color={"primary"}
+                variant={"faded"}
+            >Autocomplete
+            </Button>
+
+            <Modal isOpen={isOpen} placement="top-center" hideCloseButton={true} onOpenChange={onOpenChange} size="md">
+                <ModalContent>
+                    {(onClose) => (
+                        <>
+                            <ModalHeader className="flex flex-col p-2 m-2">Find a Commander!</ModalHeader>
+                            <ModalBody className="flex flex-col m-2 py-0">
+                                <Autocomplete
+                                    label="Select Commander"
+                                    placeholder="Type to search..."
+                                    inputProps={{
+                                        classNames: { label: "my-0 relative" }
+                                    }}
+                                    items={list.items}
+                                    inputValue={list.filterText}
+                                    isLoading={list.isLoading}
+                                    onInputChange={list.setFilterText}
+                                    onSelectionChange={selectCommander} //will call specified method with selected item's key (in this case key=item= commanders name)
+                                >
+                                    {list.items.map((item) => {
+                                        return (
+                                            <AutocompleteItem className="" key={item}>
+                                                {item}
+                                            </AutocompleteItem>
+                                        )
+                                    })}
+                                </Autocomplete>
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button color="success" variant="flat" onPress={onSuccess} > {/** Make sure whatever func this calls also
                          *  calls onOpenChange in order to close the modal */}
-                            Commander Found!
-                        </Button>
-                    </ModalFooter>
-                    </>
-                )}
-            </ModalContent>
-        </Modal>
+                                    Commander Found!
+                                </Button>
+                            </ModalFooter>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
         </>
     )
 }
