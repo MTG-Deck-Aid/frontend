6585078968eabdde89e07cdc9edc4057ae09dd99
lujarios <144175748+Lujarios@users.noreply.github.com>
2025-03-19 15:01:02 -0600

    Silly reformat

--- src/components/viewDeckContextProvider.jsx
+++ src/components/viewDeckContextProvider.jsx
@@ -17,31 +17,31 @@ export function ViewDeckContextProvider({ urlName = "New Deck", urlMode = "view"
     const [deckInput, setDeckInput] = useState('');
     const [deckList, setDeckList] = useState({});
     const [deckName, setDeckName] = useState(urlName);
-    
-    async function fetchUserDeck(){
+
+    async function fetchUserDeck() {
         // Send request to Server Side JS
         const response = await fetch(`/api/decks/deck?deckId=${urlId}`, {
             method: 'GET',
         });
-        
+
         // Await the response from server side JS
         const data = await response.json();
         return data.deck; // Return only the data
     }
 
-    function populateDeckList(deckList){
+    function populateDeckList(deckList) {
         let formattedDeckList = [];
         // Finds the total quantity of cards in the deck for each card,
         // and adds that to the card object
         deckList.forEach(card => {
             let cardIndex = formattedDeckList.findIndex((element) => element.name === card.name);
-            if(cardIndex === -1){
-                formattedDeckList.push({name: card.name, quantity: 1});
+            if (cardIndex === -1) {
+                formattedDeckList.push({ name: card.name, quantity: 1 });
             } else {
                 formattedDeckList[cardIndex].quantity += 1;
             }
         });
-        
+
         // Convert the deck list into "quantity name" string
         const deckListString = formattedDeckList.map(card => `${card.quantity} ${card.name}`).join('\n');
         console.log(deckListString);
@@ -54,13 +54,13 @@ export function ViewDeckContextProvider({ urlName = "New Deck", urlMode = "view"
     }
 
     /** DEBUGGER FUNCTION */
-    function printContext(){
+    function printContext() {
         console.log("\n\n---- View Deck Context ----");
         console.log("isEditMode: ", isEditMode);
         console.log("deckInput: ", deckInput);
         console.log("deckList: ", deckList);
         console.log("deckName: ", deckName);
-        
+
     }
 
     // On page load, fetch the deck from the backend
@@ -72,16 +72,16 @@ export function ViewDeckContextProvider({ urlName = "New Deck", urlMode = "view"
                 setDeckInput(populateDeckList(deck));
             });
         }
-    }, []); 
-    
-    
+    }, []);
+
+
     // Print the context whenever the context changes
     useEffect(() => {
-        printContext();        
+        printContext();
     }, [deckInput, deckList, deckName, isEditMode]);
 
     return (
-        <ViewDeckContext.Provider value={{ isEditMode, toggleIsEditMode, deckInput, setDeckInput, deckList, setDeckList , deckName, setDeckName, printContext}}>
+        <ViewDeckContext.Provider value={{ isEditMode, toggleIsEditMode, deckInput, setDeckInput, deckList, setDeckList, deckName, setDeckName, printContext }}>
             {children}
         </ViewDeckContext.Provider>
     );
