8d1f2ab3deb5ab65c8f6d715c58156a59ae34edf
Cody Casselman <cody.casselman@ucalgary.ca>
2025-03-20 19:38:25 -0600

    Add: dynamic title
    war is over

--- src/components/view-edit-deck/saveButton.jsx
+++ src/components/view-edit-deck/saveButton.jsx
@@ -1,24 +1,32 @@
 "use client";
-import { useEditContext, useLoadingContext } from "../context-providers/viewDeckContextProvider";
+import { useDisplayNameContext, useEditContext, useLoadingContext } from "../context-providers/viewDeckContextProvider";
 import { useDeckInputContext, useDeckListContext, useCommanderContext, useDeckNameContext } from "../context-providers/userDeckContextProvider";
 import { saveDeck } from "./utils";
 import { Button } from "@heroui/button";
+import SetPageTitle from "../header-components/setPageTitle";
+import { useDeferredValue, useEffect } from "react";
 
 export default function SaveButton(){
+    //Page context
     const {isLoading, setIsLoading} = useLoadingContext();
     const {isEditMode, toggleIsEditMode} = useEditContext();
+    const {displayName, setDisplayName} = useDisplayNameContext();
+    //Deck context
     const {deckInput, setDeckInput} = useDeckInputContext();
     const {deckList, setDeckList} = useDeckListContext();
     const {commander, setCommander} = useCommanderContext();
     const {deckName, setDeckName} = useDeckNameContext();
-
+    
     const handleSave = async () => {
         setIsLoading(true);
+        setDeckName(displayName) //set the display name when we press the save button
         await saveDeck(deckInput, setDeckInput, setDeckList, commander, deckName, toggleIsEditMode);
         setIsLoading(false);
     }
 
     return (
+        <>
+        <SetPageTitle title={deckName}/>
         <Button
             size={"md"}
             onPress={handleSave}
@@ -28,5 +36,6 @@ export default function SaveButton(){
             className=""
         >Save
         </Button>
+        </>
     )
 }
