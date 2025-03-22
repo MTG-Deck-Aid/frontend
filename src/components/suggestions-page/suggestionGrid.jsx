7a65b2273b267624e5b1596ac84b596baeb2a14f
Cody Casselman <cody.casselman@ucalgary.ca>
2025-03-21 20:05:20 -0600

    Add: simple skeleton

--- src/components/suggestions-page/suggestionGrid.jsx
+++ src/components/suggestions-page/suggestionGrid.jsx
@@ -1,12 +1,13 @@
 "use client"
 import { useSuggestionContext } from "../context-providers/suggestionContextProvider";
 import SuggestionModal from "@/components/suggestions-page/suggestionModal";
-import { Divider, ScrollShadow } from "@heroui/react";
+import { Divider, ScrollShadow, Skeleton } from "@heroui/react";
 import { useEffect, useState } from "react";
 export default function SuggestionGrid({add}){
     //props: add=(true/false). Denotes whether we use cardsToRemove or cardsToAdd
     const {suggestions} = useSuggestionContext();
     const [displayedCards, setDisplayedCards] = useState();
+    const [cardsLoaded, setCardsLoaded] = useState(false);
     //update displayed cards after we get suggestions
     useEffect(() => {
         console.log("New Suggestions:", suggestions);
@@ -20,7 +21,8 @@ export default function SuggestionGrid({add}){
     const label = (add?"Cards to Add":"Cards to Remove");
     const textContent = "Click to see suggested reasoning";
     return(
-        <div className="lg:grid lg:grid-cols-4 w-full">
+        <Skeleton isLoaded={displayedCards?true:false} className="rounded-lg">
+        <div className="lg:grid lg:grid-cols-4 w-full h-full">
             {(displayedCards)?
             <>
                 <div className="flex flex-col justify-center items-center gap-4 font-body">
@@ -33,18 +35,20 @@ export default function SuggestionGrid({add}){
                 </div>
                 <Divider className="lg:hidden"/>
                 <ScrollShadow orientation="horizontal" className="col-span-3 max-w-full">
-                    <div className="flex justify-start gap-10">
+                    <div className="flex justify-start w-full gap-10">
                     {displayedCards.map((card, index) => (
                         <SuggestionModal card={card} key={index}/>  //FOR API: card is the main prop to pass card info. We can probably add another prop like geminiResponse
                     ))}
                     </div>
                 </ScrollShadow>
             </>:
-            <div>
+            <div></div>
 
-            </div>
+            
             }
             
         </div>
+        </Skeleton>
+        
     )
 }
